a9e06f207ec96603b3342a92820ec38c4035b5e5
MazieroMarco <maziero.marco@gmail.com>
2021-01-15 15:46:11 +0100

    Added new triggers and procedures

--- data/BDR_Projet_SQL_Procedures.sql
+++ data/BDR_Projet_SQL_Procedures.sql
@@ -18,11 +18,11 @@ END//
 -- Ajout d'un doubleur
 DROP PROCEDURE IF EXISTS ajouter_doubleur;
 DELIMITER //
-CREATE PROCEDURE ajouter_utilisateur (IN nom VARCHAR(50),
-                                      IN prenom VARCHAR(50),
-                                      IN dateNaissance DATE,
-                                      IN sexe ENUM('homme', 'femme', 'autre'),
-                                      IN photoProfil VARCHAR(512))
+CREATE PROCEDURE ajouter_doubleur (IN nom VARCHAR(50),
+                                   IN prenom VARCHAR(50),
+                                   IN dateNaissance DATE,
+                                   IN sexe ENUM('homme', 'femme', 'autre'),
+                                   IN photoProfil VARCHAR(512))
 
 BEGIN
     INSERT INTO Personne VALUES (NULL, nom, prenom, dateNaissance, sexe, photoProfil);
@@ -32,12 +32,12 @@ END//
 -- Ajout d'un film
 DROP PROCEDURE IF EXISTS ajouter_film;
 DELIMITER //
-CREATE PROCEDURE ajouter_utilisateur (IN titre VARCHAR(45),
-                                      IN description TEXT,
-                                      IN duree INT,
-                                      IN image VARCHAR(512),
-                                      IN idStudioAnimation INT,
-                                      IN dateSortie DATE)
+CREATE PROCEDURE ajouter_film (IN titre VARCHAR(45),
+                               IN description TEXT,
+                               IN duree INT,
+                               IN image VARCHAR(512),
+                               IN idStudioAnimation INT,
+                               IN dateSortie DATE)
 
 BEGIN
     INSERT INTO Media VALUES (NULL, titre, description, duree, image, idStudioAnimation);
@@ -45,13 +45,13 @@ BEGIN
 END//
 
 -- Ajout d'une série
-DROP PROCEDURE IF EXISTS ajouter_film;
+DROP PROCEDURE IF EXISTS ajouter_serie;
 DELIMITER //
-CREATE PROCEDURE ajouter_utilisateur (IN titre VARCHAR(45),
-                                      IN description TEXT,
-                                      IN duree INT,
-                                      IN image VARCHAR(512),
-                                      IN idStudioAnimation INT)
+CREATE PROCEDURE ajouter_serie (IN titre VARCHAR(45),
+                                IN description TEXT,
+                                IN duree INT,
+                                IN image VARCHAR(512),
+                                IN idStudioAnimation INT)
 
 BEGIN
     INSERT INTO Media VALUES (NULL, titre, description, duree, image, idStudioAnimation);
--- data/BDR_Projet_SQL_Triggers.sql
+++ data/BDR_Projet_SQL_Triggers.sql
@@ -7,7 +7,7 @@ CREATE TRIGGER before_utilisateur_media_note_insert
     FOR EACH ROW
 BEGIN
     IF NEW.note NOT BETWEEN 1 AND 10 THEN
-        SET @s = '[table:utilisateur_media_note] - [note] column is not valid';
+        SET @s = '[table:utilisateur_media_note] - [note] column is not valid (must be between 1 and 10)';
         SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = @s;
     END IF;
 END;
@@ -22,8 +22,10 @@ CREATE TRIGGER before_film_insert
     ON film
     FOR EACH ROW
 BEGIN
-    IF EXISTS (SELECT * FROM Serie WHERE Serie.idMedia = NEW.idMedia) THEN
-        SET @s = '[table:serie] - New film overlapping with existing serie';
+    IF EXISTS (SELECT * FROM Serie
+                    WHERE Serie.idMedia = NEW.idMedia)
+    THEN
+        SET @s = '[table:film] - New film overlapping with existing serie';
         SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = @s;
     END IF;
 END;
@@ -38,7 +40,9 @@ CREATE TRIGGER before_serie_insert
     ON serie
     FOR EACH ROW
 BEGIN
-    IF EXISTS (SELECT * FROM Film WHERE Film.idMedia = NEW.idMedia) THEN
+    IF EXISTS (SELECT * FROM Film
+                    WHERE Film.idMedia = NEW.idMedia)
+    THEN
         SET @s = '[table:serie] - New serie overlapping with existing film';
         SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = @s;
     END IF;
@@ -54,8 +58,49 @@ CREATE TRIGGER before_saison_insert
     ON saison
     FOR EACH ROW
 BEGIN
-    IF EXISTS (SELECT * FROM Saison WHERE Saison.idSerie = NEW.idSerie AND NEW.num < Saison.num AND Saison.dateSortie > NEW.dateSortie) THEN
-        SET @s = '[table:serie] - New season cant be released before the old ones';
+    IF EXISTS (SELECT * FROM Saison
+                    WHERE Saison.idSerie = NEW.idSerie
+                      AND Saison.num < NEW.num
+                      AND Saison.dateSortie > NEW.dateSortie)
+    THEN
+        SET @s = '[table:saison] - New season cant be released before the old ones';
+        SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = @s;
+    END IF;
+END;
+$$
+DELIMITER ;
+
+-- Verifcation de l'ajout dans les listes d'un série qui n'est pas encore sortie
+DROP TRIGGER IF EXISTS before_list_insert;
+DELIMITER $$
+CREATE TRIGGER before_list_insert
+    BEFORE INSERT
+    ON saison
+    FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT * FROM Saison
+                    WHERE Saison.idSerie = NEW.idSerie
+                      AND Saison.num < NEW.num
+                      AND Saison.dateSortie > NEW.dateSortie)
+    THEN
+        SET @s = '[table:saison] - New season cant be released before the old ones';
+        SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = @s;
+    END IF;
+END;
+$$
+DELIMITER ;
+
+-- Ajout d'une list : nombre d'episodes vus, soit entre 0 et le nb d'episodes de la saison
+DROP TRIGGER IF EXISTS before_utilisateur_saison_insert;
+DELIMITER $$
+CREATE TRIGGER before_utilisateur_saison_insert
+    BEFORE INSERT ON utilisateur_saison
+    FOR EACH ROW
+BEGIN
+    DECLARE saisonNbEpisodes INT;
+    SELECT nbEpisodes INTO saisonNbEpisodes FROM Saison WHERE NEW.idMedia = Saison.idSerie AND NEW.numSaison = Saison.num;
+    IF NEW.nbEpisodesVus > saisonNbEpisodes THEN
+        SET @s = '[table:utilisateur_saison] - [nbEpisodesVus] column is not valid';
         SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = @s;
     END IF;
 END;
