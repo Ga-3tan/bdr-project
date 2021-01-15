6207a53a6c9e2e127abb2a4e205c3b9f11e121d0
MazieroMarco <maziero.marco@gmail.com>
2021-01-15 13:22:13 +0100

    Added lists views and requests

--- data/BDR_Projet_SQL_Requetes.sql
+++ data/BDR_Projet_SQL_Requetes.sql
@@ -1,3 +1,37 @@
+-- Recuperer l'id de l'utilisateur en fonction de son pseudo
+SELECT idPersonne FROM Utilisateur WHERE pseudo = '<pseudo>';
+
+-- Creer un nouvel utilisateur dans la base de donnees
+INSERT INTO Personne(nom, prenom) VALUES ('<prenom>', '<nom>');
+
+INSERT INTO Utilisateur(email, pseudo, password, idPersonne)
+VALUES ('<email>>', 'pseudo', '<mot de passe>', <id>);
+
+-- Verification de l'utilisateur
+SELECT password FROM Utilisateur WHERE pseudo = '<pseudo>';
+
+-- Comparaison de pseudos
+SELECT pseudo FROM Utilisateur WHERE pseudo = '<pseudo>';
+
+-- Recuperation des donnes d'un media
+SELECT * FROM vFilm WHERE id = <idMedia>
+UNION
+SELECT * FROM vSerie WHERE id = <idMedia>;
+
+-- Recuperation de la note attribuee par un utilisateur sur un media
+SELECT note, dateNote
+FROM utilisateur_media_note
+WHERE idPersonne = <idUtilisateur> AND idMedia = <idMedia>;
+
+-- Recuperation de la note moyenne d'un media
+-- Recuperation des doubleurs d'un media
+-- Recuperation des commentaires d'un media
+-- Ajout d'un commentaire sur un media
+-- Ajout d'une note sur un media
+-- Ajout d'un media dans une liste
+-- Recuperation d'une saison d'un media
+
+
 -- Sélectionner tous les médias produit par un studio
 SELECT *
 FROM Media
@@ -39,3 +73,13 @@ SELECT *
 FROM Media
    INNER JOIN vDoubleur ON Media.id = vDoubleur.id
 WHERE vDoubleur.nom = "<Nom doubleur>";
+
+-- Liste de films qui appartiennent à un utilisateur
+SELECT *
+FROM vUtilisateur_Lists_Film
+WHERE id = <userid>;
+
+-- Liste de séries qui appartiennent à un utilisateur
+SELECT *
+FROM vUtilisateur_Lists_Serie
+WHERE id = <userid>;
