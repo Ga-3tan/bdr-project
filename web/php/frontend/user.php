1a5b767bf898c178b81225fc1e04794ac10191d3
MazieroMarco <maziero.marco@gmail.com>
2021-01-17 01:36:49 +0100

    Better style in user panel

--- web/php/frontend/user.php
+++ web/php/frontend/user.php
@@ -29,36 +29,36 @@ if (!file_exists("../../img/profiles/" . $userData['photoProfil']) || $userData[
            id="profilePicture" name="update_picture"
            accept="image/png, image/jpeg">
     <ul class="w3-ul w3-large">
-        <li>
-            <input type="text" id="firstname" maxlength="50"
+        <li>First name
+            <input type="text" id="firstname" maxlength="50" class="w3-input w3-border"
                    name="update_firstname" value="<?php echo $userData['prenom']; ?>" required>
         </li>
-        <li>
-            <input type="text" id="lastname" maxlength="50"
+        <li>Last name
+            <input type="text" id="lastname" maxlength="50" class="w3-input w3-border"
                    name="update_lastname" value="<?php echo $userData['nom']; ?>" required>
         </li>
-        <li>Birthday: <span><input type="date" id="start" name="update_birthdate"
-                                   min="1900-01-01" max="2999-12-31"
+        <li>Birthday<span><input type="date" id="start" name="update_birthdate"
+                                   min="1900-01-01" max="2999-12-31" class="w3-input w3-border"
                                    value="<?php echo $userData['dateNaissance']; ?>" required></span></li>
         <li>
-            <input type="text" id="username" maxlength="60"
+            <input type="text" id="username" maxlength="60" class="w3-input w3-border"
                    name="update_username" value="<?php echo $userData['pseudo']; ?>" required>
         </li>
         <li>
-            sexe: <span>
-                <select id="updateGender" name="update_gender">
+            Gender<span>
+                <select id="updateGender" name="update_gender" class="w3-select w3-border">
                     <option value="homme" <?php echo $userData['sexe'] == 'homme' ? 'selected' : ''; ?> >Man</option>
                     <option value="femme" <?php echo $userData['sexe'] == 'femme' ? 'selected' : ''; ?> >Woman</option>
                     <option value="autre" <?php echo $userData['sexe'] == 'autre' ? 'selected' : ''; ?>>Other</option>
                 </select>
         </li>
         <li>
-            email : <span><input type="email" id="email"
-                                 name="update_email"
+            Email<span><input type="email" id="email"
+                                 name="update_email" class="w3-input w3-border"
                                  value="<?php echo $userData['email']; ?>" required></span>
         </li>
         <li>
-            role : <span><?php echo $userData['moderateur'] ? 'Moderateur' : 'Utilisateur'; ?></span>
+            Account type<span><?php echo $userData['moderateur'] ? 'Moderateur' : 'Utilisateur'; ?></span>
         </li>
         <li>
             <button class="w3-button w3-blue" type="submit" value="Send" name="update_send">Submit modifications</button>
@@ -67,7 +67,7 @@ if (!file_exists("../../img/profiles/" . $userData['photoProfil']) || $userData[
         <li style="background-color: black"></li>
         <?php
         if ($userData['moderateur']) {
-            echo '<li>
+            echo '<li>Moderator options
                     <form action="admin_panel.php">
                         <input class="w3-button w3-blue" type="submit" value="Add content in database" />
                     </form>
